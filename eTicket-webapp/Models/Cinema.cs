47e6ced8e7aa4079b9d81fa2bee1eb0e7eff13d4
Alfredo Nieto <Nieto.Alfredo@outlook.com>
2023-05-26 08:49:06 -0500

    Added Cinemas view

--- eTicket-webapp/Models/Cinema.cs
+++ eTicket-webapp/Models/Cinema.cs
@@ -6,9 +6,11 @@ namespace eTicket_webapp.Models
     {
         [Key]
         public int CinemaId { get; set; }
-
+        [Display(Name = "Cinema Logo")]
         public string Logo { get; set; }
+        [Display(Name = "Cinema Name")]
         public string Name { get; set; }
+        [Display(Name = "Cinema Description")]
         public string Description { get; set; }
 
         public List<Movie> Movies { get; set; }
